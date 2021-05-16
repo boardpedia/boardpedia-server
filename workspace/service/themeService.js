bff9634d78c5549e0de926c467763848c8511974
sophryu99 <sehwa_ryu@berkeley.edu>
2021-05-16 23:07:56 +0900

    [update] 저장여부, 저장수, 평점 계산 로직 함수화

--- workspace/service/themeService.js
+++ workspace/service/themeService.js
@@ -1,4 +1,6 @@
 const { Boardgame, Theme, Saved, Review, User } = require('../models');
+const commonService = require('../service/commonService')
+
 const { search } = require('../routes');
 const sequelize = require('sequelize');
 const Op = sequelize.Op;
@@ -30,6 +32,7 @@ module.exports = {
             throw error;
         }
     },
+    
 
     /* 테마 상세 조회 */
     getThemeDetail: async (UserIdx, ThemeIdx) => {
@@ -45,6 +48,7 @@ module.exports = {
                 where:{
                     themeIdx: ThemeIdx,
                 },
+                attributes: ['ThemeIdx', 'name', 'detail', 'imageUrl', 'tag'], 
             });
             for (i = 0; i < themes.length; i++) {
                 var res = themes[i].tag.split(";");
@@ -74,8 +78,6 @@ module.exports = {
 
             savedGameCount.count = savedGameCount.count
 
-            console.log(savedGameCount.count, 'yesssss')
-
             const reviews = await Review.findAll({
                 attributes: ['GameIdx', 'star']
             })
@@ -84,59 +86,17 @@ module.exports = {
             //     group: ['star'],
             // })
 
-            for (i = 0; i < searchedGame.length; i++) { 
-                // 'saved': 저장 여부 파라미터 추가해주기
-                searchedGame[i].dataValues.saved = 0;
-
-                // 'saveCount': 저장 횟수 파라미터 추가해주기
-                searchedGame[i].dataValues.saveCount = 0;
-
-                // 'star: 별점 파라미터 추가해주기'
-                searchedGame[i].dataValues.star = 0;
-                var cnt = 0
-
-                // 저장 여부 탐색하기
-                for (j = 0; j < savedGame.length; j++) { 
-                    if (searchedGame[i].GameIdx == savedGame[j].GameIdx) {
-                        searchedGame[i].dataValues.saved = 1;
-                        console.log(searchedGame[i].GameIdx, savedGame[j].GameIdx)
-                    } else {
-                        searchedGame[i].dataValues.saved = 0;
-                    }
-                }
-
-                // 게임 별 평점 계산하기
-                for (j = 0; j < reviews.length; j++) { 
-                    if (searchedGame[i].GameIdx == reviews[j].GameIdx) {
-                        cnt ++
-                        searchedGame[i].dataValues.star = searchedGame[i].dataValues.star + reviews[j].star
-                    } 
-                }
-                if (cnt > 0) {
-                    searchedGame[i].dataValues.star =  searchedGame[i].dataValues.star / cnt
-                }
-
-                // 저장 카운트 매핑해주기
-                for (j = 0; j < savedGameCount.length; j++) { 
-                    if (searchedGame[i].GameIdx == savedGameCount[j].GameIdx) {
-                        console.log(searchedGame[i].GameIdx, savedGameCount[j].GameIdx)
-                        console.log(savedGameCount[j].count)
-                        searchedGame[i].dataValues.saveCount = searchedGame[i].dataValues.saveCount + savedGameCount[j].count
-                    }
-                }
-                
-            }
-
+            const themeGame = await commonService.getSavedCountReview(searchedGame, savedGame, savedGameCount, reviews)
             const result = ({
                 themes,
-                searchedGame
+                themeGame
             });
-
             return result;
         } catch (error) {
             throw error;
         }
     },
+
     
 
 }
